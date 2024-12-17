926dfa30abcf77036cce68d7ee78cdf1c44cfb6c
Dinesh442000 <dchoudhary442000@gmail.com>
2024-12-17 19:59:52 +0530

    push context data

--- src/test/java/cucumberOptions/TestNGRunner.java
+++ src/test/java/cucumberOptions/TestNGRunner.java
@@ -3,8 +3,9 @@ package cucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/features", glue="stepDefinitions",
-monochrome=true, tags="@Mortgage or @NetBanking")
+@CucumberOptions(features="src/test/java/features",dryRun=true, glue="stepDefinitions",
+monochrome=true, tags="@Mortgage or @NetBanking" , plugin= {"pretty","html:target/cucumber.html",
+		"json:target/cucumberjson.json"})
 public class TestNGRunner extends AbstractTestNGCucumberTests{
 
 }
